1de9d42555a588c5eda285cf2e58f6ea3824e59c
toronja <garbagesolid@gmail.com>
2019-03-07 10:58:56 -0600

    Agregada la clase Schedule para llamar de funciones a varios instantes de tiempos dados

--- scripts/SpellcardManager.gd
+++ scripts/SpellcardManager.gd
@@ -1,15 +1,27 @@
 extends Node
 signal spellcard_ended
 
-var _spellcard_data = []
+onready var DeffaultShootsScript = preload("res://scripts/DefaultShoots.gd")
+onready var ScheduleScript = preload("res://scripts/Schedule.gd")
 var _frame_counter = 0
-# Un arreglo que almacena datos de las spellcards
+var _spells = []
+var _DeffShoots = null
+var _affected_group = null
+var _character = null
 
 func load_spellcard_data():
-	pass
+	var spell = ScheduleScript.Schedule.new()
+	var pos = _character.global_position
+	spell.add_stop(2, _DeffShoots, "bullet_a", [pos, _affected_group, GlobalScript.HALF_PI, 400])
+	# Generar Bala tipo A a los 2 segundos
+	spell.add_stop(2, _DeffShoots, "bullet_a", [pos, _affected_group, -GlobalScript.HALF_PI, 400])
+	spell.add_stop(2, _DeffShoots, "bullet_a", [pos, _affected_group, GlobalScript.QUARTER_PI, 400])
+	spell.add_stop(2, _DeffShoots, "bullet_a", [pos, _affected_group, -GlobalScript.QUARTER_PI, 400])
+	_spells.append(spell)
 
 func _ready():
-	var character = get_node("../")
+	_character = get_node("../")
+	_DeffShoots = DeffaultShootsScript.new()
 	self.set_process(false)
 
 func _process(delta):
