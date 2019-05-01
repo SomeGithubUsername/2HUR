666e735975e506c585e1e38b1e16bdff53bf4c4c
Toronja <garbagesolid@gmail.com>
2019-05-01 01:04:25 -0500

    Agregado el prototipo de un jefe de pruebas

---
+++ scripts/DummyBoss.gd
@@ -0,0 +1,6 @@
+extends "res://scripts/Boss.gd"
+
+func _init():
+	._init()
+	self.add_spell("test", 500, {"Life":1, "Score":12, "Power":3})
+	self.add_spell("test2", 500, {"Life":1, "Score":12, "Power":3})
